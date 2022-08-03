e8917c31b3e70e37ed5739888fe59e19c1e56c6e
Justyna Janczyszyn <justyna.janczyszyn@gmail.com>
2022-08-03 14:38:01 +0200

    import fix

--- migrator/migrator.py
+++ migrator/migrator.py
@@ -2,7 +2,7 @@
 
 import click
 
-from commands import initdb, revision, up
+from .commands import initdb, revision, up
 
 
 @click.group(help="CLI tool to manage migrations")
@@ -12,7 +12,7 @@ def cli() -> None:
 
 cli.add_command(initdb.initdb)
 cli.add_command(up.up)
-cli.add_command(revision.revision)
+# cli.add_command(revision.revision)
 
 if __name__ == '__main__':
     cli()
